90e7065e16832c0db13a8d1f6141d15bd1c50390
Jagadeeshcdev <89693846+Jagadeeshcdev@users.noreply.github.com>
2023-07-24 17:44:48 +0530

    efb.py

--- efb.py
+++ efb.py
@@ -1,47 +1,47 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import re
 
 def scrape_email_from_facebook(url):
-    # Set up Chrome options to run headless (without GUI) to avoid opening a visible browser window
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-
-    # Initialize the Chrome WebDriver
-    driver = webdriver.Chrome(options=chrome_options)
-
     try:
-        driver.get(url)
-        time.sleep(5)  # Wait for the page to load (you may adjust the time if needed)
-
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
         # Find the elements with CSS selector ".uiScaledImageContainer" and get their attributes
-        elements = driver.find_elements_by_css_selector(".uiScaledImageContainer")
+        elements = soup.select(".uiScaledImageContainer")
         emails = []
 
         for element in elements:
-            email = element.get_attribute("aria-label")
+            email = element.get("aria-label")
             if email == "No email address found.":
                 continue
             emails.append(email)
 
         return emails
 
-    except Exception as e:
-        print(f"Error occurred while scraping the email: {str(e)}")
-
-    finally:
-        driver.quit()
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error occurred while scraping the email: {str(e)}")
 
 def main():
-    url = "https://www.facebook.com/kcmplumbingandheating/"
-    emails = scrape_email_from_facebook(url)
-    if emails:
-        print("Emails found:")
-        for email in emails:
-            print(email)
-    else:
-        print("No email address found on the page.")
+    st.title("Facebook Email Scraper")
+
+    # User input: URL
+    st.text("Enter the URL of the Facebook page you want to scrape:")
+    url = st.text_input("URL", "https://www.facebook.com/kcmplumbingandheating/")
+
+    # Scrape email
+    if st.button("Scrape Email"):
+        st.text("Scraping email, please wait...")
+        emails = scrape_email_from_facebook(url)
+        
+        if emails:
+            st.success("Emails found:")
+            for email in emails:
+                st.write(email)
+        else:
+            st.warning("No email address found on the page.")
 
 if __name__ == "__main__":
     main()
