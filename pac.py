a0a43d21e994d777e55f35225413c212abee1ccd
Jagadeeshcdev <89693846+Jagadeeshcdev@users.noreply.github.com>
2023-07-24 17:47:03 +0530

    pac.py

--- pac.py
+++ pac.py
@@ -5,3 +5,7 @@ import pandas as pd
 import time
 from io import StringIO
 from selenium import webdriver
+from scrapy.crawler import CrawlerRunner
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+import re
